7fc563a8c7f4a533cf70b6214e2f0212b0679043
Marcos Ramirez <mrcrmz@gmail.com>
2020-03-23 13:49:36 -0700

    image test

--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -11,7 +11,7 @@
             <a class="email" href="mailto:mrcrmz@gmail.com">mrcrmz@gmail.com</a>
         </div>
         <br/>
-        <img alt="Marcos at work" src="@/assets/images/contact/marcos_computer.JPG" height="250">
+        <img alt="River vibes" src="@/assets/images/about/marcos_river.png" height="400">
     </div>
 </template>
 
